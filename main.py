382d8dcded3d29f3b0bf376ed819845901308867
ellenealds <elle.neal@bpaquality.co.uk>
2022-11-22 12:35:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,42 +4,15 @@
 import streamlit as st
 import pandas as pd
 
-# create a class that stores the data and the output of the function
-class Data:
-    def __init__(self, data):
-        self.data = data
-        self.output = None
-        # the class can create add columns based on a function
-        # the function is called on each row of the data
-        # the output is stored in the output attribute
-    def add_column(self, function):
-        self.output = self.data.apply(function, axis=1)
-        return self.output
-
+# the user can either upload their own file in the format of a csv containing one column called data or they can use the sample data
+# import csv file
+df = pd.read_csv('fedex_reviews.csv')
 # upload the csv file
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 if uploaded_file is not None:
-    # read the csv file into a pandas dataframe
     df = pd.read_csv(uploaded_file)
-    # create an instance of the Data class
-    data = Data(df)
-    # add a column to the dataframe
-    data.add_column(function)
-
-    # display the dataframe into a table with filter and sort options
-    st.write(data.output)
-
-# import csv file
-df = pd.read_csv('fedex_reviews.csv')
-
-# if no file is uploaded, add a button to use sample data
-if uploaded_file is None:
-    if st.button('Use sample data'):
-        uploaded_file = df
-
-# create an instance of the Data class
-data = Data(df)
-
-#display the dataframe into a table with filter and sort options
-st.write(data.output)
+else :
+    df = uploaded_file
+    
+    st.write(df)
 
